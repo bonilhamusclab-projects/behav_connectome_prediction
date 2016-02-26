e6d7b846779a9e23e97b10444cccc5c85f5c8999
lbuser <lbuser@noreply.github.com>
2016-02-26 00:15:20 -0500

    ignore invalid ids

--- step2_convert_excel.py
+++ step2_convert_excel.py
@@ -1,7 +1,27 @@
 from __future__ import division
 
+import re
+
 import numpy as np
 import pandas as pd
+
+
+def valid_id(id):
+    """
+    >>> from step2_convert_excel import valid_id
+    >>> valid_id("M1234")
+    True
+    >>> valid_id("VA")
+    False
+    >>> import numpy as np
+    >>> valid_id(np.NaN)
+    False
+
+    """
+    if isinstance(id, basestring):
+        return re.match(r"M[0-9]{3,4}", id) is not None
+    else:
+        return False
 
 
 def convert_excel(src_file, dest_path):
@@ -33,5 +53,13 @@ def convert_excel(src_file, dest_path):
     for c in numeric_cols:
         data[c] = [float_or_null(i) for i in data[c]]
 
+    valid_indices = [valid_id(i) for i in data['id']]
+    data = data.loc[valid_indices, :]
+
     data.to_csv(dest_path, index=False)
     return data
+
+
+def _test():
+    import doctest
+    doctest.testfile("step2_convert_excel.py")
