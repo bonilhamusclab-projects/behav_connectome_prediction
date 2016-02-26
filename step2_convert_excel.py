55460f307977dd321a071a1b83bb319b06958fc2
lbuser <lbuser@noreply.github.com>
2016-02-25 23:24:34 -0500

    step2 converts all non-numeric to null

--- step2_convert_excel.py
+++ step2_convert_excel.py
@@ -1,5 +1,6 @@
 from __future__ import division
 
+import numpy as np
 import pandas as pd
 
 
@@ -17,6 +18,20 @@ def convert_excel(src_file, dest_path):
     data = excel[column_mappings.keys()]
 
     data.rename(columns=column_mappings, inplace=True)
+
+    def float_or_null(i):
+        try:
+            return float(i)
+        except:
+            return np.NaN
+
+    numeric_cols = ['pd_avg_total_words',
+                    'pd_avg_total_different_words',
+                    'avg_total_words',
+                    'avg_different_words']
+
+    for c in numeric_cols:
+        data[c] = [float_or_null(i) for i in data[c]]
 
     data.to_csv(dest_path, index=False)
     return data
