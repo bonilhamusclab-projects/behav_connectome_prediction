286a363a878943b089eb61c911d6f270ab576bcf
lbuser <lbuser@noreply.github.com>
2016-03-23 12:37:47 -0400

    use pd as covar

--- explore_significant_regressors.jl
+++ explore_significant_regressors.jl
@@ -41,7 +41,15 @@ function create_edge_names(edges::Vector{Symbol})
   map(mk_edge_string, edges)
 end
 
-function calc_coefs(df::DataFrame, target::Symbol, edges::Vector{Symbol})
+
+macro update_covar(k, v)
+  :(
+    covar_dt[symbol(c, "_", $k)][ix] = $v
+  )
+end
+
+function calc_coefs(df::DataFrame, target::Symbol, edges::Vector{Symbol},
+                    covars::Vector{Symbol}=Symbol[])
 
   num_edges = length(edges)
 
@@ -52,21 +60,53 @@ function calc_coefs(df::DataFrame, target::Symbol, edges::Vector{Symbol})
   pvalues = ones(Float64, num_edges)
   tscores = mk_zeros()
 
+  num_covars = length(covars)
+  covar_dt = begin
+    mk_ones = () -> ones(Float64, num_edges)
+    ret = Dict{Symbol, Vector{Float64}}()
+    for c in covars
+      for (t, fn) in (("coef", mk_zeros), ("pvalue", mk_ones), ("tscore", mk_zeros))
+        k::Symbol = symbol(c, "_", t)
+        ret[k] = fn()
+      end
+    end
+
+    ret
+  end
+
   for (ix, edge) = enumerate(edges)
     if all(df[edge] .== 0.)
       continue
     end
     fm_str = "$target ~ $edge"
+    if !isempty(covars)
+      fm_str = string(fm_str, " + ", join(covars, " + "))
+    end
     fm = eval(parse(fm_str))
     ct = coeftable(lm(fm, df))
     coefs[ix], tscores[ix], pvalues[ix] = ct.mat[2, [1, 3, 4]]
+
+    for c in covars
+      c_c, c_t, c_p = ct.mat[3, [1, 3, 4]]
+      @update_covar :coef c_c
+      @update_covar :tscore c_t
+      @update_covar :pvalue c_p
+    end
+
     cors[ix] = cor(df[edge], df[target])
   end
 
   edge_names = create_edge_names(edges)
-  ret = DataFrame(coef=coefs, pvalue=pvalues, tscore=tscores, edge=edges, edge_name=edge_names, cor_coef=cors)
 
+  ret = DataFrame(coef=coefs, pvalue=pvalues, tscore=tscores, edge=edges, edge_name=edge_names, cor_coef=cors)
   ret[:pvalue_adj] = padjust(ret[:pvalue], BenjaminiHochberg)
+
+  for c in keys(covar_dt)
+    ret[c] = covar_dt[c]
+    if contains("$c", "pvalue")
+      ret[symbol(c, "_adj")] = padjust(ret[c], BenjaminiHochberg)
+    end
+  end
 
   ret
 end
@@ -83,23 +123,26 @@ end
 
 function calc_all()
 
-  data_targets = [(full_adw, :adw_z), (full_atw, :atw_diff_wpm), (full_atw, :atw_z)]
+  data_targets = [(full_adw, :se_adw, [:pd_adw]), (full_atw, :se_atw, [:pd_atw])]
 
   ret = Dict()
 
-  for (data, target) = data_targets
+  for (data, target, covars) = data_targets
 
-    function calc_sorted(data::DataFrame, edges::Vector{Symbol})
-      sort(calc_coefs(data, target, edges), cols=:pvalue_adj)
+    function calc_sorted(edges::Vector{Symbol})
+      sort(calc_coefs(data, target, edges, covars), cols=:pvalue_adj)
     end
 
-    println(target)
     edges = get_edges(data)
-    ret[target] = calc_sorted(data, get_edges(data))
+    println(target)
+
+    ret[target] = calc_sorted(edges)
+
     println("left")
-    ret[symbol(target, "_left")] = calc_sorted(data, filter(is_left_hemi_edge, edges))
+    ret[symbol(target, "_left")] = calc_sorted(filter(is_left_hemi_edge, edges))
+
     println("left select")
-    ret[symbol(target, "_left_select")] = calc_sorted(data, filter(is_left_hemi_select_edge, edges))
+    ret[symbol(target, "_left_select")] = calc_sorted(filter(is_left_hemi_select_edge, edges))
   end
 
   ret
