a162a3f7a6350b66718a38207ca3fd4fcaf2b571
lbuser <lbuser@noreply.github.com>
2016-03-23 20:11:48 -0400

    calculates groups through macros

--- explore_significant_regressors.jl
+++ explore_significant_regressors.jl
@@ -4,6 +4,8 @@ using Memoize
 using PValueAdjust
 using PyCall
 
+@enum Measure atw adw
+
 full_adw = readtable("data/step3/full_adw.csv")
 full_atw = readtable("data/step3/full_atw.csv")
 
@@ -15,11 +17,13 @@ jhu_left_names = Set(jhu_left[:name])
 jhu_left_select = readtable("data/jhu_rois_left_adjusted.csv")
 jhu_left_select_names = Set(jhu_left_select[:name])
 
+
 function get_edges(df::DataFrame)
   filter(n -> startswith(string(n), "x"), names(df))
 end
 
 edge_reg = r"x([0-9]+)_([0-9]+)"
+
 
 @memoize function edge_col_to_x(edge::Symbol, x::Symbol)
   m = match(edge_reg, string(edge))
@@ -29,14 +33,17 @@ edge_reg = r"x([0-9]+)_([0-9]+)"
   end
 end
 
+
 @memoize function edge_col_to_roi_names(edge::Symbol)
   edge_col_to_x(edge, :name)
 end
+
 
 @memoize function mk_edge_string(edge::Symbol)
   left, right = edge_col_to_roi_names(edge)
   "$(left) -- $(right)"
 end
+
 
 function create_edge_names(edges::Vector{Symbol})
   map(mk_edge_string, edges)
@@ -48,6 +55,7 @@ macro update_covar(k, v)
     covar_dt[symbol(c, "_", $k)][ix] = $v
   )
 end
+
 
 function calc_coefs(df::DataFrame, target::Symbol, edges::Vector{Symbol},
                     covars::Vector{Symbol}=Symbol[])
@@ -112,43 +120,128 @@ function calc_coefs(df::DataFrame, target::Symbol, edges::Vector{Symbol},
   ret
 end
 
+
 @memoize function is_left_hemi_edge(edge_col::Symbol)
   a, b = edge_col_to_roi_names(edge_col)
   in(a, jhu_left_names) & in(b, jhu_left_names)
 end
+
 
 @memoize function is_left_hemi_select_edge(edge_col::Symbol)
   a, b = edge_col_to_roi_names(edge_col)
   in(a, jhu_left_select_names) & in(b, jhu_left_select_names)
 end
 
-function calc_all()
 
-  data_targets = [(full_adw, :se_adw, [:pd_adw]), (full_atw, :se_atw, [:pd_atw])]
+type DataTarget
+  full::DataFrame
+  target::Symbol
+  covars::Vector{Symbol}
+  rows_filter::Function
+end
+
+
+DataTarget(full::DataFrame, target::Symbol;
+           covars::Vector{Symbol}=Symbol[],
+           rows_filter::Function=df -> repmat([true], size(df, 1))) =
+  DataTarget(full, target, covars, rows_filter)
+
+Base.start(d::DataTarget) = 1
+Base.getindex(d::DataTarget, s::Symbol) = d.(s)
+Base.next(d::DataTarget, state) = d[fieldnames(DataTarget)[state]], state + 1
+Base.done(d::DataTarget, state) = state > length(fieldnames(DataTarget))
+
+function calc_all(data_targets::Vector{DataTarget})
 
   ret = Dict()
 
-  for (data, target, covars) = data_targets
+  for (data, target, covars, rows_filter) in data_targets
+
+    valid_rows::Vector{Bool} = rows_filter(data)
+    data = data[valid_rows, :]
 
     function calc_sorted(edges::Vector{Symbol})
-      sort(calc_coefs(data, target, edges, covars), cols=:pvalue_adj)
+      sort(calc_coefs(data, target, edges, covars), cols=:pvalue)
     end
 
     edges = get_edges(data)
-    println(target)
 
-    ret[target] = calc_sorted(edges)
+    k::Symbol = symbol(join([target; covars], "_cv_"))
+    println(k)
+
+    ret[k] = calc_sorted(edges)
 
     println("left")
-    ret[symbol(target, "_left")] = calc_sorted(filter(is_left_hemi_edge, edges))
+    ret[symbol(k, "_left")] = calc_sorted(filter(is_left_hemi_edge, edges))
 
     println("left select")
-    ret[symbol(target, "_left_select")] = calc_sorted(filter(is_left_hemi_select_edge, edges))
+    ret[symbol(k, "_left_select")] = calc_sorted(filter(is_left_hemi_select_edge, edges))
   end
 
   ret
-
 end
+
+
+function update_expr(e::Expr, old_term, new_term)
+  parse(replace(
+          string(e), old_term, new_term
+          ))
+end
+
+
+macro eval_str(s)
+  quote
+    eval(parse($s))
+  end
+end
+
+
+macro calc_all()
+  quote
+    function calc_dt_diff(m::Measure)
+      full = @eval_str "full_$m"
+      target = @eval_str ":$(m)_diff_wpm"
+      DataTarget(full, target, rows_filter=rows_filter_gen(m))
+    end
+
+    function calc_dt_covar(m::Measure)
+      full = @eval_str "full_$m"
+      target = @eval_str ":se_$m"
+      covars = @eval_str "[:pd_$m]"
+      DataTarget(full, target, covars=covars, rows_filter = rows_filter_gen(m))
+    end
+
+    data_targets = [calc_dt_covar(adw), calc_dt_covar(atw),
+                    calc_dt_diff(adw), calc_dt_diff(atw)]
+    calc_all(data_targets)
+  end
+end
+
+
+function calc_all_all_subjects()
+  rows_filter_gen(m::Measure) = d::DataFrame -> repmat([true], size(d, 1))
+
+  @calc_all
+end
+
+
+function calc_all_improved_subjects()
+  @memoize diff_col(m::Measure) = symbol(m, "_diff_wpm")
+
+  rows_filter_gen(m::Measure) =  d::DataFrame -> d[diff_col(m)] .> 0
+
+  @calc_all
+end
+
+
+function calc_all_poor_pd_subjects()
+  @memoize pd_z_col(m::Measure) = symbol("pd_", m, "_z")
+
+  rows_filter_gen(m::Measure) = d::DataFrame -> d[pd_z_col(m)] .< -1
+
+  @calc_all
+end
+
 
 function save_calc_all_results(res::Dict, dir::AbstractString)
   for k in keys(res)
@@ -156,6 +249,7 @@ function save_calc_all_results(res::Dict, dir::AbstractString)
     writetable("$dir/$k.csv", df)
   end
 end
+
 
 function load_local_py(name)
   (name, ext) = rsplit(name, '.';limit=2)
