963f2dcf0b0c00667b399bef83cb333d11b4e1f9
lbuser <lbuser@noreply.github.com>
2016-04-06 19:10:31 -0400

    step3 consolidate lesions

--- step3_consolidate_data.py
+++ step3_consolidate_data.py
@@ -6,13 +6,13 @@ import numpy as np
 import pandas as pd
 
 
-def _prep_df(df):
+def _prep_conn_df(df):
     """
     >>> import pandas as pd
     >>> import numpy as np
     >>> data = np.random.rand(3, 3)
-    >>> from step3_consolidate_data import _prep_df
-    >>> ret = _prep_df(pd.DataFrame(data))
+    >>> from step3_consolidate_data import _prep_conn_df
+    >>> ret = _prep_conn_df(pd.DataFrame(data))
     >>> expected_rows = ['0_1', '0_2', '1_2']
     >>> assert np.all(np.sort(ret.index) == expected_rows)
     >>> get_val = lambda c1, c2: ret[ret.index=='%s_%s' % (c1, c2)]['value'].iloc[0]
@@ -40,21 +40,25 @@ def _prep_df(df):
     return ret
 
 
-def load_x_csv(csv_path):
-    df = _prep_df(pd.read_csv(csv_path, header=None))
+def load_conn_csv(csv_path):
+    df = _prep_conn_df(pd.read_csv(csv_path, header=None))
     df = df.T
     df.index = [os.path.basename(csv_path)[:-4]]
     return df
 
 
-def consolidate_x(csv_dir="data/step1/", dest_path="data/step3/X.csv"):
+def load_lesion_csv(csv_path):
+    return pd.read_csv(csv_path, header=None).T
+
+
+def consolidate_x(csv_dir, dest_path, load_fn):
     fs = os.listdir(csv_dir)
     num_fs = len(fs)
 
     def load(csv, wait=dict(wait=0)):
         wait['wait'] += 1
         print "%s out of %s" % (wait['wait'], num_fs)
-        return load_x_csv(os.path.join(csv_dir, csv))
+        return load_fn(os.path.join(csv_dir, csv))
 
     dfs = pd.concat([load(c) for c in fs], axis=0)
     assert dfs.shape[0] == num_fs
@@ -65,7 +69,17 @@ def consolidate_x(csv_dir="data/step1/", dest_path="data/step3/X.csv"):
     return dfs
 
 
-def consolidate(y_dir="data/step2/",
+def consolidate_conn_x(csv_dir="data/step1/conn", dest_path="data/step3/conn/X.csv"):
+    print("conn")
+    consolidate_x(csv_dir, dest_path, load_conn_csv)
+
+
+def consolidate_lesion_x(csv_dir="data/step1/lesion", dest_path="data/step3/lesion/X.csv"):
+    print("lesion")
+    consolidate_x(csv_dir, dest_path, load_lesion_csv)
+
+
+def consolidate_x_with_y(y_dir="data/step2/",
                 x_src="data/step3/X.csv",
                 dest_dir="data/step3/"):
     x = pd.read_csv(x_src, index_col=0)
@@ -89,6 +103,24 @@ def consolidate(y_dir="data/step2/",
         ret[yo] = full
 
     return ret
+
+
+def consolidate_lesion_x_with_y(x_src="data/step3/lesion/X.csv",
+                       dest_dir="data/step3/lesion/"):
+    consolidate_x_with_y(x_src=x_src, dest_dir=dest_dir)
+
+
+def consolidate_conn_x_with_y(x_src="data/step3/conn/X.csv",
+                       dest_dir="data/step3/conn/"):
+    consolidate_x_with_y(x_src=x_src, dest_dir=dest_dir)
+
+
+def run_all():
+    consolidate_conn_x()
+    consolidate_conn_x_with_y()
+
+    consolidate_lesion_x()
+    consolidate_lesion_x_with_y()
 
 
 def _test():
